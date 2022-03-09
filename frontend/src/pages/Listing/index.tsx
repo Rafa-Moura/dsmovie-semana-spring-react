2f6ed8ec562552a2123554eb2178e9cf2fd4e41b
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-03-09 19:46:44 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,34 @@
-import Pagination from 'components/pagination';
-import React from 'react';
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
 
-
-
-const Listing: React.FC = () => {
+function Listing() {
   return (
-    <Pagination />
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
